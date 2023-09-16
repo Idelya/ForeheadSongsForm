f0d0113891d1c0785e12edf1a856af38aedc3a1b
unknown <annapapros21@gmail.com>
2023-09-16 12:49:54 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -38,19 +38,19 @@ function App() {
             <Typography>
               Szukamy piosenek do nasze (<i>lepszej</i>) wersji czółka. Jest to
               wersja gry na nucenie - przypomina ona kalambury, ale zamiast
-              pokazywania jest nucenie. Z tego powodu potrzebujemy piosenek.
+              pokazywania jest nucenie.
               <br /> <br />
               Dobra piosenka dla czółka to taka która jest w miarę znana i która
               ma w sobie coś charakterystycznego. Trudno zgadywać piosenki kiedy
-              każda z nich brzmi tak samo :) Im więcej haseł tym lepsza zabawa.
-              Dla tego dodawajcie swoje propozycje.
+              każda z nich brzmi tak samo :) Im więcej haseł tym lepsza zabawa,
+              dlatego dodawajcie swoje propozycje.
               <br /> <br />
               Dodając postarajcie się uważać na ortografię. W ten sposób
-              aplikacja być <i>może</i> wykryje jeżeli dana piosenka już
-              istnieje. <br />
-              Nie dodawajcie proszę głupot. Nie zamierzamy przeglądac haseł,
+              aplikacja <i>może</i> wykryje jeżeli dana piosenka już istnieje.{" "}
+              <br />
+              Nie dodawajcie proszę głupot. Nie zamierzamy przeglądać haseł,
               żeby też moć zagrać. <br />
-              Co oznacza, że to
+              To oznacza że to
               <b> WY kontrolujecie poprawność dodawanych haseł. </b>.
               <br />
               <br />W miarę możliwości dla piosenek z bajek podawajcie polskie
